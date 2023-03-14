ba19f0379cad7f9aed1d77f80254ff5d819ce188
pleasesailas <pleasesailas@gmail.com>
2023-03-14 17:40:33 +0800

    Initialize Project

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const exphdbs = require('express-handlebars')
+const restaurant = require('./restaurant.json')
+const restaurantList = restaurant.results
+
+// setting view engine
+app.engine('handlebars', exphdbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// setting route for static
+app.use(express.static('public'))
+
+// routing
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurantList })
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  res.render('show', { restaurant: restaurant })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword.toLowerCase().trim()
+  const restaurants = restaurantList.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword)
+  })
+
+  res.render('index', { restaurants: restaurants, keyword: keyword })
+})
+
+//listening
+app.listen(port, () => {
+  console.log(`Express is listening on http://localost:${port}`)
+})
