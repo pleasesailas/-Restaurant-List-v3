e7c8edc428ef97730c8c3673b0658dfd66ef63cb
pleasesailas <pleasesailas@gmail.com>
2023-03-29 11:41:51 +0800

    feat: add detail function

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.get('/', (req, res) => {
     .then(restaurants => res.render('index', { restaurants }))
     .catch(error => console.log(error))
 })
-
+// Create
 app.get('/restaurants/new', (req, res) => {
   res.render('new')
 })
@@ -48,9 +48,14 @@ app.post('/restaurants', (req, res) => {
     .catch(error => console.log(error))
 })
 
-app.get('/restaurants/:restaurant_id', (req, res) => {
-  const restaurant = restaurantList.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
-  res.render('show', { restaurant: restaurant })
+app.get('/restaurants/:id', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render('show', { restaurant }))
+    .catch(error => console.log(error))
+  // const restaurant = restaurantList.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  // res.render('show', { restaurant: restaurant })
 })
 
 app.get('/search', (req, res) => {
