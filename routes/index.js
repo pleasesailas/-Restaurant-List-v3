f5dd077cd25cbb7d3c1b0ece056fa3fb17c1c521
pleasesailas <pleasesailas@gmail.com>
2023-04-26 09:29:01 +0800

    feat: add users routes & modify banner size

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router()
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 const search = require('./modules/search')
+const users = require('./modules/users')
 
-router.use('/', home)
+router.use('/users', users)
 router.use('/restaurants', restaurants)
 router.use('/search', search)
+router.use('/', home)
 
 module.exports = router
